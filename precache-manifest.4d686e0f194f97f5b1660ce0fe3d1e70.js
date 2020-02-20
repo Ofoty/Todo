617a7918d35e045bfaca34fbc4e06a63fbb1f170
Ofoty <53044034+Ofoty@users.noreply.github.com>
2020-02-20 22:10:15 +0500

    Add files via upload

---
+++ precache-manifest.4d686e0f194f97f5b1660ce0fe3d1e70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cba3df58e409b2bb8ba591d9c5120b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13cc85a2523637f11ef4",
+    "url": "/static/css/main.375ed5cc.chunk.css"
+  },
+  {
+    "revision": "d0304108c376e7fffbaf",
+    "url": "/static/js/2.9a933809.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9a933809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2972506e76c3d55182d9",
+    "url": "/static/js/3.4398c0bf.chunk.js"
+  },
+  {
+    "revision": "13cc85a2523637f11ef4",
+    "url": "/static/js/main.4f24f506.chunk.js"
+  },
+  {
+    "revision": "d4dcca93fd3695aa45a8",
+    "url": "/static/js/runtime-main.1c3a22b4.js"
+  }
+]);
